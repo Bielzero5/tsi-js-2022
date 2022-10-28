3e23cc91eec6f8caaa9b73a08102e536dfb636fb
GABRIEL PEREIRA FERREIRA <gabriel.pferreira7@senacsp.edu.br>
2022-10-28 19:56:03 -0300

    to-do, adicionar, apagar e filtrar funcionando

--- formulario/appListaDeTarefas/script.js
+++ formulario/appListaDeTarefas/script.js
@@ -7,15 +7,43 @@ const btnLimpaTudo = document.querySelector(".limpar-tarefas");
 function carregaMonitoresDeEventos() {
   //evento para adicionar a tarefa
   btnAddTarefa.addEventListener("click", adicionarTarefa);
-  //evento para fazer o x funcionar (apagar uma Ãºnica tarefa)
+  //evento para fazer o x funcionar (apagar uma única tarefa)
   listaDeTarefas.addEventListener("click", apagarTarefa);
-
+  //evento para limpar toda a lista
+  btnLimpaTudo.addEventListener("click", apagarTudo);
+  //evento para filtrar as tarefas
   filtroDeTarefa.addEventListener("keyup", filtrar);
 }
 carregaMonitoresDeEventos();
 
 function filtrar(evento) {
-  alert("Salve");
+  //capturamos o que o usuário está digitando
+  //passando tudo para minúsculo
+  const procurado = evento.target.value.toLowerCase();
+  //capturamos todos os elementos <li> existentes
+  const tarefas = document.querySelectorAll(".collection-item");
+
+  //Para cada tarefa existente, busque a string
+  //desejada
+  tarefas.forEach(function (tarefa) {
+    //Recuperamos apenas o texto do elemento
+    //<li> onde está a tarefa
+    textoTarefa = tarefa.innerText;
+
+    //Procuramos a string digitada no filtro
+    //no texto que está no <li>
+    if (textoTarefa.toLowerCase().indexOf(procurado) != -1) {
+      tarefa.style.display = "block";
+    } else {
+      tarefa.style.display = "none";
+    }
+  });
+}
+
+function apagarTudo(evento) {
+  evento.preventDefault();
+
+  listaDeTarefas.innerHTML = "";
 }
 
 function apagarTarefa(evento) {
@@ -29,37 +57,50 @@ function apagarTarefa(evento) {
   }
 }
 
-function limparTarefas(e) {
-  e.preventDefault();
-  listaDeTarefas.innerHTML = "";
-}
-
 function adicionarTarefa(evento) {
   evento.preventDefault();
 
-  //verifica se usuÃ¡rio entrou com uma tarefa
+  //verifica se usuário entrou com uma tarefa
   if (entradaTarefa.value === "") {
     alert("Entre com uma tarefa");
-  } else {
-    //cria li com a nova tarefa
-    const li = document.createElement("li");
-    li.className = "collection-item";
-    li.appendChild(document.createTextNode(entradaTarefa.value));
-
-    //cria a onde vai ficar o x para apagar a tarefa
-    const a = document.createElement("a");
-    a.className = "apaga-tarefa secondary-content";
-
-    //cria Ã­cone com x para apagar a tarefa
-    const i = document.createElement("i");
-    i.className = "fa fa-remove";
-
-    //monta o elemento li para colocar em ul
-    a.appendChild(i);
-    li.appendChild(a);
-    listaDeTarefas.appendChild(li);
-
-    //apaga o input para entrada
-    entradaTarefa.value = "";
+    return false;
   }
+
+  //cria li com a nova tarefa
+  const li = document.createElement("li");
+  li.className = "collection-item";
+  li.appendChild(document.createTextNode(entradaTarefa.value));
+
+  //cria a onde vai ficar o x para apagar a tarefa
+  const a = document.createElement("a");
+  a.className = "apaga-tarefa secondary-content";
+
+  //cria I­cone com x para apagar a tarefa
+  const i = document.createElement("i");
+  i.className = "fa fa-remove";
+
+  //monta o elemento li para colocar em ul
+  a.appendChild(i);
+  li.appendChild(a);
+  listaDeTarefas.appendChild(li);
+
+  //gravar a tarefa no localStorage
+  gravarTarefa(entradaTarefa.value);
+
+  //apaga o input para entrada
+  entradaTarefa.value = "";
+}
+
+function gravarTarefa(tarefa) {
+  let tarefas = [];
+
+  let tarefaDoStorage = localStorage.getItem("tarefas");
+
+  if (tarefaDoStorage != null) {
+    tarefas = JSON.parse(tarefaDoStorage);
+  }
+
+  tarefas.push(tarefa);
+
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
 }
